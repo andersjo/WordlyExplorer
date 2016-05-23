79153c5b2c2d0ad5c0e71ab2d2895dc9045da8c6
dirkhovy <mail@dirkhovy.com>
2016-05-23 13:49:34 +0200

    normalzation per region, and doubvle results page

--- flaskr/humboldt.py
+++ flaskr/humboldt.py
@@ -43,7 +43,10 @@ def search():
     Displays the search page accessible at '/search'
     """
     if request.method == 'POST':
-        return do_single_search(request.form)
+        if len(request.form) == 2:
+            return do_single_search(request.form)
+        elif len(request.form) == 3:
+            return do_double_search(request.form)
 
     else:
         return flask.render_template('search.html',
@@ -99,16 +102,21 @@ def do_single_search(request_form):
 
     total_found = TOTALS[TOTALS['country_code'] == country_var]['count'].sum()
 
+
     gender_buckets = buckets_to_series(json_results['facets']['genders']['buckets'])
     gender_buckets = gender_buckets[[val for val in gender_buckets.index if val != NOT_AVAIL]] / total_found
     age_buckets = buckets_to_series(json_results['facets']['ages']['buckets']) / total_found
     age_gender_buckets = compound_bucket_to_series(json_results['facets']['gender_and_age']['buckets'],
                                                    'gender_and_age')
     age_gender_buckets['count'] /= total_found
-    age_gender_buckets = age_gender_buckets[(age_gender_buckets['age'] <= MAX_AGE) & (age_gender_buckets['age'] >=MIN_AGE) & (age_gender_buckets['gender'] != NOT_AVAIL)].sort('age')
+    age_gender_buckets = age_gender_buckets[
+        (age_gender_buckets['age'] <= MAX_AGE) & (age_gender_buckets['age'] >= MIN_AGE) & (
+        age_gender_buckets['gender'] != NOT_AVAIL)].sort('age')
 
     # TODO: get total count per region and normalize by that?
-    nuts_buckets = (buckets_to_series(json_results['facets']['nuts_3_regions']['buckets']) / total_found).to_json()
+    nuts_buckets = buckets_to_series(json_results['facets']['nuts_3_regions']['buckets'])
+    nuts_totals = TOTALS.groupby('nuts_3').sum()
+    nuts_buckets = (nuts_buckets / nuts_totals.ix[nuts_buckets.index, :]['count'].fillna(1).values).to_json()
 
     # TODO move plotting to its own function
     gender_plot = Bar(gender_buckets,
@@ -123,7 +131,7 @@ def do_single_search(request_form):
                    logo=None,
                    toolbar_location="below",
                    width=800,
-                   height=400)
+                   height=400, webgl=True)
 
     age_gender_plot = Bar(age_gender_buckets,
                           group='gender',
@@ -135,10 +143,10 @@ def do_single_search(request_form):
                           width=1200,
                           height=400,
                           legend='top_right',
-                          color=['blue', 'green'])
+                          color=['blue', 'green'], webgl=True)
 
-
-    bokeh_script, (gender_plot_div, age_plot_div, age_gender_plot_div) = components((gender_plot, age_plot, age_gender_plot))
+    bokeh_script, (gender_plot_div, age_plot_div, age_gender_plot_div) = components(
+        (gender_plot, age_plot, age_gender_plot))
 
     return flask.render_template('single_term_results.html',
                                  query=request_form["singleTermQuery"],
@@ -260,39 +268,18 @@ def do_double_search(request_form):
                                         language_code=language_var,
                                         country_code=country_var)
 
-    total_found = json_results1['facets']['count'] + json_results2['facets']['count']
 
-    # TODO: what is our denominator? Number of matches or total population???
-    gender_buckets = buckets_to_series(json_results1['facets']['genders']['buckets']) / total_found
-    age_buckets = buckets_to_series(json_results1['facets']['ages']['buckets']) / total_found
 
-    print(gender_buckets)
-    print(age_buckets)
-    print()
+    # bokeh_script, (gender_plot_div, age_plot_div) = components((gender_plot, age_plot))
 
-    # TODO move plotting to its own function
-    gender_plot = Bar(gender_buckets,
-                      title="Gender distribution",
-                      logo=None,
-                      toolbar_location="below",
-                      width=300,
-                      height=400)
-
-    age_plot = Bar(age_buckets,
-                   title="Age distribution",
-                   logo=None,
-                   toolbar_location="below",
-                   width=800,
-                   height=400)
-
-    bokeh_script, (gender_plot_div, age_plot_div) = components((gender_plot, age_plot))
-
-    return flask.render_template('double_term_results.html',
-                                 query=request_form["singleTermQuery"],
-                                 bokeh_script=bokeh_script,
-                                 gender_plot=gender_plot_div,
-                                 age_plot=age_plot_div,
-                                 json_results=json.dumps(json_results1, indent=True),
+    return flask.render_template('comparison_term_results.html',
+                                 query1=request_form["doubleTermQuery1"],
+                                 query2=request_form["doubleTermQuery2"],
+                                 # bokeh_script=bokeh_script,
+                                 # gender_plot=gender_plot_div,
+                                 # age_plot=age_plot_div,
+                                 json_results1=json.dumps(json_results1, indent=True),
+                                 json_results2=json.dumps(json_results2, indent=True),
                                  country_code=country_var,
                                  map_views=MAP_VIEWS,
                                  available_options=AVAILABLE_OPTIONS
