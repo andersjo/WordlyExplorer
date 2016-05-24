fd782449dc3ec7818b658944a2f7e9e989a1adc1
dirkhovy <mail@dirkhovy.com>
2016-05-24 23:20:26 +0200

    comparison stuff

--- flaskr/config.py
+++ flaskr/config.py
@@ -10,7 +10,6 @@ MIN_AGE = 16
 MAX_AGE = 81
 P_LEVELS = [0.01, 0.05, 0.1]
 
-
 # list of language code, country code, language name, and country name
 AVAILABLE_OPTIONS = [('da', 'dk', 'Danish', 'Denmark'), ('en', 'uk', 'English', 'UK')]#, ('fr', 'fr', 'French', 'France')]
 
