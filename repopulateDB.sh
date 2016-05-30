861fb88244eb2feda361ca807f9a9549cc6f32e6
dirkhovy <mail@dirkhovy.com>
2016-05-30 12:04:06 +0200

    index

--- repopulateDB.sh
+++ repopulateDB.sh
@@ -1,3 +1,5 @@
 curl localhost:8983/solr/humboldt/update?commit=true -d '<delete><query>*:*</query></delete>'
 python scripts/import_reviews.py ~/Dropbox/Humboldt/united_kingdom.auto-adjusted_gender.NUTS-regions.CoNLL.jsonl --locations ~/Dropbox/Humboldt/mapped_locations.tsv --country-code uk
 python scripts/import_reviews.py ~/Dropbox/Humboldt/denmark.auto-adjusted_gender.NUTS-regions.CoNLL.jsonl --locations ~/Dropbox/Humboldt/mapped_locations.tsv --country-code dk
+python scripts/import_reviews.py ~/Dropbox/Humboldt/germany.auto-adjusted_gender.NUTS-regions.CoNLL.jsonl --locations ~/Dropbox/Humboldt/mapped_locations.tsv --country-code de
+python scripts/import_reviews.py ~/Dropbox/Humboldt/france.auto-adjusted_gender.NUTS-regions.CoNLL.jsonl --locations ~/Dropbox/Humboldt/mapped_locations.tsv --country-code fr
